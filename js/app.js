f65717f7064af3fce921eb76412f076de4fca053
Jyotish <takingnovembersat@gmail.com>
2016-06-19 16:21:18 +0530

    Partial Blog layout
    Pending:
    1. Back botton on navbar
    2. Logo color adjustment
    3. Social plugins
    4. Styling images and other embeds inside content box
    5. For now, I don't remember more than this!

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var myElement = document.querySelector("nav");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement, {
+  "offset": 250,
+  "tolerance": 3,
+});
+// initialise
+headroom.init(); 
